a27b5a2702cc37bcb5c2bb619519b824dd557e76
netakiryosuke <sigoryosuke@gmail.com>
2025-12-29 06:43:43 +0900

    Add fixed_effects_analysis_service class

---
+++ backend/app/domain/service/fixed_effects_analysis_service.py
@@ -0,0 +1,15 @@
+class fixed_effects_analysis_service:
+    def __init__(
+            self,
+            dependent_var: str,
+            independent_vars: list[str],
+            entity_var: str,
+            time_var: str,
+    ):
+        self.dependent_var = dependent_var
+        self.independent_vars = independent_vars
+        self.entity_var = entity_var
+        self.time_var = time_var
+
+    def analyze(self, dataframe) -> dict:
+        pass
