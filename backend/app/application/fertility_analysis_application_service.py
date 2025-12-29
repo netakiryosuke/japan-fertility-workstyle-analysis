76513f08c80850601c34ed2b96482265e9c75ad7
netakiryosuke <sigoryosuke@gmail.com>
2025-12-29 20:14:33 +0900

    Add application service class

---
+++ backend/app/application/fertility_analysis_application_service.py
@@ -0,0 +1,23 @@
+class FertilityAnalysisApplicationService:
+    def __init__(self):
+        self.csv_loader = CsvDataFrameLoader()
+
+    def analyze_from_csv(
+        self,
+        csv_bytes: bytes,
+        dependent_var: str,
+        independent_vars: list[str],
+        entity_var: str = "prefecture",
+        time_var: str = "year"
+    ):
+        df = self.csv_loader.load(csv_bytes)
+
+        analysis_service = FixedEffectsAnalysisService(
+            dependent_var=dependent_var,
+            independent_vars=independent_vars,
+            entity_var=entity_var,
+            time_var=time_var
+        )
+
+        result = analysis_service.analyze(df)
+        return result
