3756d410c07fd6248a08c8d7a82e4900f18efa78
netakiryosuke <sigoryosuke@gmail.com>
2025-12-29 17:28:08 +0900

    create analyze method in FixedEffectsAnalysisService

--- backend/app/domain/service/fixed_effects_analysis_service.py
+++ backend/app/domain/service/fixed_effects_analysis_service.py
@@ -1,3 +1,5 @@
+from linearmodels.panel import PanelOLS
+
 class FixedEffectsAnalysisService:
     def __init__(
             self,
@@ -12,4 +14,24 @@ class FixedEffectsAnalysisService:
         self.time_var = time_var
 
     def analyze(self, dataframe) -> dict:
-        pass
+        df = dataframe.set_index([self.entity_var, self.time_var])
+
+        y = df[self.dependent_var]
+        x = df[self.independent_vars]
+
+        fixed_effects_model = PanelOLS(y, x, entity_effects=True)
+
+        result = fixed_effects_model.fit()
+
+        return {
+            "nobs": int(result.nobs),
+            "params": result.params.to_dict(),
+            "std_errors": result.std_errors.to_dict(),
+            "tstats": result.tstats.to_dict(),
+            "pvalues": result.pvalues.to_dict(),
+            "rsquared": {
+                "within": result.rsquared_within,
+                "between": result.rsquared_between,
+                "overall": result.rsquared_overall,
+            }
+        }
