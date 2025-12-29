cce8fd2aff604881dac3fad1a1664fdcaf775485
Ryosuke Osawa <sigoryosuke@gmail.com>
2025-12-29 17:56:40 +0900

    Add fixed effects analysis domain service (#3)
    * Add fixed_effects_analysis_service class
    
    * Fix class name
    
    * create analyze method in FixedEffectsAnalysisService
    
    * Add FixedEffectsResult class
    
    * Fix return of analyze method in fixed_effects_analysis_service
    
    * Fix value type in fixed_effects_result

---
+++ backend/app/domain/model/fixed_effects_result.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class FixedEffectsResult:
+    nobs: int
+    params: dict[str, float]
+    std_errors: dict[str, float]
+    tstats: dict[str, float]
+    pvalues: dict[str, float]
+    rsquared_within: float
+    rsquared_between: float
+    rsquared_overall: float
---
+++ backend/app/domain/service/fixed_effects_analysis_service.py
@@ -0,0 +1,38 @@
+from linearmodels.panel import PanelOLS
+
+from backend.app.domain.model.fixed_effects_result import FixedEffectsResult
+
+
+class FixedEffectsAnalysisService:
+    def __init__(
+            self,
+            dependent_var: str,
+            independent_vars: list[str],
+            entity_var: str,
+            time_var: str,
+    ):
+        self.dependent_var = dependent_var
+        self.independent_vars = independent_vars
+        self.entity_var = entity_var
+        self.time_var = time_var
+
+    def analyze(self, dataframe) -> FixedEffectsResult:
+        df = dataframe.set_index([self.entity_var, self.time_var])
+
+        y = df[self.dependent_var]
+        x = df[self.independent_vars]
+
+        fixed_effects_model = PanelOLS(y, x, entity_effects=True)
+
+        result = fixed_effects_model.fit()
+
+        return FixedEffectsResult(
+            nobs=int(result.nobs),
+            params=result.params.to_dict(),
+            std_errors=result.std_errors.to_dict(),
+            tstats=result.tstats.to_dict(),
+            pvalues=result.pvalues.to_dict(),
+            rsquared_within=result.rsquared_within,
+            rsquared_between=result.rsquared_between,
+            rsquared_overall=result.rsquared_overall
+        )
