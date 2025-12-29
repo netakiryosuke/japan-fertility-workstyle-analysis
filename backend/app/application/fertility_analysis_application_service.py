bdb97f7c47dd2c85b1656626a1b6725421a1f53e
Ryosuke Osawa <sigoryosuke@gmail.com>
2025-12-29 20:52:42 +0900

    Add application service (#5)
    * remove unused file
    
    * Add application service class
    
    * Fix return
    
    * Minor changes
    
    * Add return type hints
    
    * Minor changes
    
    * Import CsvDataFrameLoader class
    
    * Import FixedEffectsAnalysisService class
    
    * Add dependency injection

---
+++ backend/app/application/fertility_analysis_application_service.py
@@ -0,0 +1,28 @@
+from backend.app.domain.model.fixed_effects_result import FixedEffectsResult
+from backend.app.domain.service.fixed_effects_analysis_service import FixedEffectsAnalysisService
+from backend.app.infrastructure.csv_dataframe_loader import CsvDataFrameLoader
+
+
+class FertilityAnalysisApplicationService:
+    def __init__(self, csv_loader):
+        self.csv_loader = csv_loader or CsvDataFrameLoader()
+
+    def analyze(
+        self,
+        csv_bytes: bytes,
+        dependent_var: str,
+        independent_vars: list[str],
+        entity_var: str = "prefecture",
+        time_var: str = "year"
+    ) -> FixedEffectsResult:
+
+        dataframe = self.csv_loader.load(csv_bytes)
+
+        analysis_service = FixedEffectsAnalysisService(
+            dependent_var=dependent_var,
+            independent_vars=independent_vars,
+            entity_var=entity_var,
+            time_var=time_var
+        )
+
+        return analysis_service.analyze(dataframe)
