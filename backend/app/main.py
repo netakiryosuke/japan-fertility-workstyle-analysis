f02d6ff08dfcc7b00964d96d8bac41ec97e182ef
netakiryosuke <sigoryosuke@gmail.com>
2025-12-29 21:34:45 +0900

    Include router in main.py (root)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from backend.app.api.main import api_router
+
 app = FastAPI(
     title="Japan Fertility Workstyle Analysis API",
 )
+
+app.include_router(api_router)
 
 @app.get("/health")
 def health_check():
