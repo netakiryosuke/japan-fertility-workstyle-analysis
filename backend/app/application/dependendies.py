63fe53cc7fc6f5933f165276aae1c02eefb7ae1c
netakiryosuke <sigoryosuke@gmail.com>
2025-12-31 19:22:00 +0900

    Add dependencies.py in application layer

---
+++ backend/app/application/dependendies.py
@@ -0,0 +1,8 @@
+from fastapi import Depends
+from backend.app.application.fertility_analysis_application_service import FertilityAnalysisApplicationService
+from backend.app.infrastructure.dependencies import get_csv_dataframe_loader
+
+def get_fertility_analysis_application_service(
+    csv_loader = Depends(get_csv_dataframe_loader),
+) -> FertilityAnalysisApplicationService:
+    return FertilityAnalysisApplicationService(csv_loader=csv_loader)
