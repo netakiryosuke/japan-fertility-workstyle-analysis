9070df476b36b911da1c723b83186d4f80c5ff70
netakiryosuke <sigoryosuke@gmail.com>
2026-01-01 05:54:12 +0900

    Fix error message in missing columns exception

--- backend/app/application/exception/missing_columns_exception.py
+++ backend/app/application/exception/missing_columns_exception.py
@@ -1,4 +1,4 @@
 class MissingColumnsException(Exception):
     def __init__(self, missing_columns: list[str]):
         self.missing_columns = missing_columns
-        super().__init__(f"Required columns are missing: {missing_columns}")
+        super().__init__(f"Required columns are missing in CSV file: {missing_columns}")
